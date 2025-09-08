bad4c0c1888f6573e999ddc061a5822d242b2bc2
AbrilOrtega2 <aortega2@mail.austral.edu.ar>
2025-09-08 13:01:09 -0300

    Add files via upload

--- number_to_month.py
+++ number_to_month.py
@@ -1,4 +1,27 @@
-# Replace the "ANSWER HERE" for your answer
-
-def number_to_month(month):
-    return "ANSWER HERE" # Remove this line and implement
+def number_to_month(number):
+    if number ==1: 
+        return ("enero")
+    if number ==2:
+        return ("febrero")
+    elif number ==3:
+        return ("marzo")
+    elif number ==4:
+        return ("abril")
+    elif number ==5:
+        return ("mayo")
+    elif number ==6:
+        return ("junio")
+    elif number ==7:
+        return ("julio")
+    elif number ==8:
+        return ("agosto")
+    elif number ==9:
+        return ("septiembre")
+    elif number ==10:
+        return ("octubre")
+    elif number ==11:
+        return ("noviembre")
+    elif number ==12:
+        return ("diciembre")
+    else:
+        return 'error'
